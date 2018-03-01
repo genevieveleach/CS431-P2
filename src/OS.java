e0f27a65cd67928625ec573057ed9de85c2822e2
Rachael Shima <rachael_shima@hotmail.com>
2018-02-28 19:38:44 -0800

    Update OS.java
    Brainstorm Session Notes

--- src/OS.java
+++ src/OS.java
@@ -1,2 +1,12 @@
 public class OS {
+  pointer to array values in page table
+  ReplacePageEntry()
+  int pointer
+  ReadPage()
+    - reads pagefile and loads it into memory
+      -.pg
+  WritePage()
+    - write to .pg file from Physical memory
+    - reset d bit
+  ResetR()
 }
