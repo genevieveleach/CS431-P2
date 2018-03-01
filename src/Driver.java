6f1a6a0239cc3812eadc73893dd0d2a66e18c114
Rachael Shima <rachael_shima@hotmail.com>
2018-02-28 19:07:32 -0800

    Update Driver.java
    Added Print lines

--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,8 @@
 public class Driver {
+  PrintStream csv = new PrintStream(new FileOutputStream("Output.txt");
+  System.setOut(csv);
+  System.out.println("Address,r/w,value,soft,hard,hit,evicted_pg#,dirty_evicted_page");
+  While (){
+    System.out.println(address + "," + rw + "," + value + "," + soft + "," hard + "," + hit + "," + evicted + "," + dirty);
+  }
 }
