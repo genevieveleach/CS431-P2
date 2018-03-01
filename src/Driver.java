a275421b3290f03714b602c85558de94d575e00f
Rachael Shima <rachael_shima@hotmail.com>
2018-02-28 19:31:02 -0800

    Update Driver.java
    Took out while loop

--- src/Driver.java
+++ src/Driver.java
@@ -7,11 +7,7 @@ public class Driver {
   int hit;
   String evicted;
   String dirty;
-  boolean stop = false;
-  PrintStream csv = new PrintStream(new FileOutputStream("Output.txt");
+  PrintStream csv = new PrintStream(new FileOutputStream("Output.txt"));
   System.setOut(csv);
   System.out.println("Address,r/w,value,soft,hard,hit,evicted_pg#,dirty_evicted_page");
-  While (stop != true){
-    System.out.println(address + "," + rw + "," + value + "," + soft + "," hard + "," + hit + "," + evicted + "," + dirty);
-  }
 }
