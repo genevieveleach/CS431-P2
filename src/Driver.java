e267215abb76082ec9949105877100f00714e5d3
Rachael Shima <rachael_shima@hotmail.com>
2018-03-01 10:12:48 -0800

    Update Driver.java
    Added file reader, feel free to make the .util and .io more specialized

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java,io.*;
+
 public class Driver {
   String address;
   int rw;
@@ -10,4 +13,7 @@ public class Driver {
   PrintStream csv = new PrintStream(new FileOutputStream("Output.txt"));
   System.setOut(csv);
   System.out.println("Address,r/w,value,soft,hard,hit,evicted_pg#,dirty_evicted_page");
+  String path = args[0];
+  File file = new File(path);
+  Scanner input = new Scanner(file);
 }
