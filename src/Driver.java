0a3b1809a581b293f70a34549c403f007207040d
Rachael Shima <rachael_shima@hotmail.com>
2018-02-28 19:13:01 -0800

    Update Driver.java
    Set variables

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,17 @@
 public class Driver {
+  String address;
+  int rw;
+  String value;
+  int soft;
+  int hard;
+  int hit;
+  String evicted;
+  String dirty;
+  boolean stop = false;
   PrintStream csv = new PrintStream(new FileOutputStream("Output.txt");
   System.setOut(csv);
   System.out.println("Address,r/w,value,soft,hard,hit,evicted_pg#,dirty_evicted_page");
-  While (){
+  While (stop != true){
     System.out.println(address + "," + rw + "," + value + "," + soft + "," hard + "," + hit + "," + evicted + "," + dirty);
   }
 }
