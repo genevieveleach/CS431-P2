d2a6e3ba780a9aeb888ff2ea89ef419a6ab48fd5
Rachael Shima <rachael_shima@hotmail.com>
2018-03-03 17:07:49 -0800

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -11,12 +11,12 @@ public class Driver {
     int hit;
     String evicted;
     String dirty;
-    PrintStream csv = new PrintStream(new FileOutputStream("Output.txt"));
-    System.setOut(csv);
-    System.out.println("Address,r/w,value,soft,hard,hit,evicted_pg#,dirty_evicted_page");
     String path = args[0];
     File file = new File(path);
     Scanner input = new Scanner(file);
+    PrintStream csv = new PrintStream(new FileOutputStream("Output.txt"));
+    System.setOut(csv);
+    System.out.println("Address,r/w,value,soft,hard,hit,evicted_pg#,dirty_evicted_page");
   }
 
 }
